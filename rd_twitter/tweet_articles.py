edc5d22cc69c10e20ccdf131e3d1faf3f1683a1f
unknown <Terrence_Beach@CHAT_QA_102.nuance.com>
2012-06-01 15:40:27 -0400

    Updates to tweet_articles.py

--- rd_twitter/tweet_articles.py
+++ rd_twitter/tweet_articles.py
@@ -29,16 +29,13 @@ import common
 TWEET_SHIFT = 8
 
 # The first tweet to be sent
-TWEET_GREETING = "Hello I will start tweeting now"
+TWEET_GREETING = "" 
 
 # The last tweet to be sent
-TWEET_SIGN_OFF = "Goodbye I'm done tweeting for now"
+TWEET_SIGN_OFF = ""
 
 # A list of stock tweets to inject inbetween article tweets
-STOCK_TWEETS = [
-    "This is stock tweet 1",
-    "This is stock tweet 2"
-]
+STOCK_TWEETS = []
 
 
 ###############################################################################
@@ -63,12 +60,15 @@ def main():
     auth = tweepy.OAuthHandler(TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET)
     auth.set_access_token(TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_TOKEN_SECRET)
     twitter_api = tweepy.API(auth)
-    
+
     # Get the list of articles to tweet (if any)
     if not os.path.exists("new_tweets.txt"):
         log.error("new_tweets.txt is missing.  Please run build_new_tweets.py script first.")
         return
-    infile = codecs.open("new_tweets.txt", "r", "utf8")
+    try:
+        infile = codecs.open("new_tweets.txt", "r", "utf8")
+    except:
+        infile = open("new_tweets.txt", "r")
     articles = []
     for line in infile:
         article = line.rstrip()
@@ -77,7 +77,7 @@ def main():
     # Build the list of tweets
     interval = len(articles) / len(STOCK_TWEETS)
     counter = 1
-    tweets = [TWEET_GREETING]
+    tweets = []
     for article in articles:
         tweets.append(article)
         if counter == interval:
@@ -89,22 +89,32 @@ def main():
     tweets.append(TWEET_SIGN_OFF)
     
     # Tweet the articles
+    send_tweet(log, twitter_api, "Sending Greeting - Tweet contents:", TWEET_GREETING)
     log.info("Sending %i new tweets" % len(tweets))
     counter = 1
     wait_time = TWEET_SHIFT * 3600 / len(tweets)
     for tweet in tweets:
+        log.info("Waiting %i minutes before sending next tweet" % (wait_time / 60))
+        time.sleep(wait_time)
+        send_tweet(log, twitter_api, "Sending tweet %i - Tweet contents:" % counter, tweet)
+        counter += 1
+
+
+def send_tweet(log, twitter_api, message, tweet):
+    retry = 0
+    while True:
+        if retry > 9:
+            log.error("Maximum number of retries reached")
+            return
         try:
-            log.info("Sending tweet %i - Tweet contents:" % counter)
+            log.info(message)
             log.info(tweet)
             twitter_api.update_status(tweet)
+            return
         except:
-            log.error("Problem sending tweet %i" % counter)
-        counter += 1
-        if counter == len(tweets):
-            continue
-        log.info("Waiting %i seconds before sending next tweet" % wait_time)
-        time.sleep(wait_time)
-
+            log.error("Problem sending tweet. Retrying...")
+            time.sleep(5.0)
+            retry += 1
 
 if __name__ == "__main__":
     main()
